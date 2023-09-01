4fcf02c7dfdcdd258fece39e57d869ab4472ad01
Kanishka Mishra <65494925+KanishkaMishra10@users.noreply.github.com>
2023-09-01 19:29:53 +0530

    Added DTO files

---
+++ EComm.Core.dto/RequestModel/CartRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Core.dto.RequestModel
+{
+    public class CartRequestModel
+    {
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
